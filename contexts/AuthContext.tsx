f7f068234cb39fc8b5cbe6fa688ef4127000234f
Mjeed101 <Mjeed102@proton.me>
2026-04-28 12:26:34 +0300

    fix: login return type Promise<User>

--- contexts/AuthContext.tsx
+++ contexts/AuthContext.tsx
@@ -20,7 +20,7 @@ interface User {
 interface AuthContextType {
   user: User | null;
   isLoading: boolean;
-  login: (email: string, password: string) => Promise<void>;
+  login: (email: string, password: string) => Promise<User>;
   logout: () => Promise<void>;
   setUser: (user: User | null) => void;
 }
